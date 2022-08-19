cfd6c3caba311e5f5daf340bb1f266118bcee33a
Akpa Onyedikachukwu <akpa3220@gmail.com>
2022-08-19 10:16:52 +0100

    cart

--- test.js
+++ test.js
@@ -3,7 +3,36 @@ let cart = Array.from (document.querySelectorAll("#cart"));
 cart.map(
     (button)=>{
         button.addEventListener("click",(e)=>{
-            
+
+            let clip = e.target.parentElement.parentElement.parentElement;
+
+            if(clip.classList.contains("list")){
+                var image = clip.childNodes[1].childNodes[1].childNodes[1].childNodes[1].src;
+
+                //console.log(image)
+
+                var proName = clip.childNodes[3].childNodes[1].textContent;
+                //console.log(proName)
+
+                var price = clip.childNodes[3].childNodes[3].childNodes[1].textContent;
+                //console.log(price)
+
+                let noir = [image,proName,price]
+
+                console.log(noir)
+
+                let cart;
+
+                if(localStorage.getItem("cart")===null){
+                    cart = []
+                }
+                else{
+                    cart = JSON.parse(localStorage.getItem("cart"))
+                }
+                cart.push(noir)
+                localStorage.setItem("cart",JSON.stringify(cart))
+            }
+
         })
     }
 )
