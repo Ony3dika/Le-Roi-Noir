ca01e6c15ad47125599aaa3ae06e880ee2ef588a
Akpa Onyedikachukwu <akpa3220@gmail.com>
2022-08-25 08:41:31 +0100

    cart

--- cart.js
+++ cart.js
@@ -1,9 +1,12 @@
-let pros = document.querySelector(".pros")
+let pros = document.querySelector(".pros");
 
 document.addEventListener("DOMContentLoaded", cartLoad)
 
 function cartLoad(){
-    if (localStorage.getItem("cart")===null){
+
+    let cart;
+    
+      if (localStorage.getItem("cart")===null){
         cart=[]
     }
     else{
@@ -109,7 +112,12 @@ function cartLoad(){
         <svg class="pos" xmlns="http://www.w3.org/2000/svg"  width="30" height="30" fill="currentColor" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M384 32C419.3 32 448 60.65 448 96V416C448 451.3 419.3 480 384 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H384zM224 368C237.3 368 248 357.3 248 344V280H312C325.3 280 336 269.3 336 256C336 242.7 325.3 232 312 232H248V168C248 154.7 237.3 144 224 144C210.7 144 200 154.7 200 168V232H136C122.7 232 112 242.7 112 256C112 269.3 122.7 280 136 280H200V344C200 357.3 210.7 368 224 368z"/></svg>
         </div>
 
-        <div class="text-center pat"><button style="background-color:#ffd39e;color: black;" class="px-xl-2 px-1 rounded mt-3 ms-xl-3 exit"  id="exit">REMOVE</button></div>
+        <div class="text-center pat">
+
+        <button style="background-color:#ffd39e;color: black;" class="px-xl-2 px-1 rounded mt-3 ms-xl-3 exit" id="remove">REMOVE</button>
+        
+        </div>
+        
         </div>
 
         <div class="col-3">
@@ -117,7 +125,7 @@ function cartLoad(){
         </div>
 
         <div class="col-3">
-            <h5 class="text-center mt-5">$${mul}.00</h5>
+            <h5 class="text-center mt-5" id="tilt">$${mul}.00</h5>
         </div>
 
         <hr>`
@@ -125,42 +133,27 @@ function cartLoad(){
 
     })
 
-    let dele = document.querySelectorAll("#exit")
+    let btn = Array.from (document.querySelectorAll("#remove"))
 
-    console.log(dele)
+    //console.log(btn)
 
-    //Use Id to map \(°O°)/
-
-    dele.forEach((tick)=>{
-        tick.addEventListener("click",(e)=>{
-
+    btn.map((rem)=>{
+        rem.addEventListener("click", (e,index)=>{
             let cart;
 
-            if (localStorage.getItem("cart"===null)){
+            if(localStorage.getItem("cart")===null){
                 cart = []
             }
-
             else{
                 cart = JSON.parse(localStorage.getItem("cart"))
             }
 
-            //console.log(50)
-
-            cart.forEach((stuff,index)=>{
-
-
-                if(2+2 ==4){
-                    console.log(stuff)
-                    //cart.splice(index, 1)
-                }else{
-                    console.log(stuff[1])
-                }
+            btn.map((stuff)=>{
+                console.log(stuff)
             })
+        }) 
+    })   
+ }
 
-            //localStorage.setItem("cart",JSON.stringify(cart))
-        })
-    })
  
-}
-
 
